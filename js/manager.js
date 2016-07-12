66c8ddc6777c9e0913e5ae96167cc9c9eedee37b
nadine-shi <nadine@afterlogic.com>
2016-07-12 08:57:39 +0300

    AdminPanel - temporary exclude Licensing and Logging tabs

--- js/manager.js
+++ js/manager.js
@@ -21,21 +21,21 @@ module.exports = function (oAppData, iUserRole, bPublic) {
 					Settings.HashModuleName + '-db',
 					TextUtils.i18n('CORECLIENT/LABEL_DB_SETTINGS_TAB')
 				]);
-				ModulesManager.run('AdminPanelClient', 'registerAdminPanelTab', [
-					function () { return require('modules/CoreClient/js/views/LicensingAdminSettingsView.js'); },
-					Settings.HashModuleName + '-licensing',
-					TextUtils.i18n('CORECLIENT/LABEL_LICENSING_SETTINGS_TAB')
-				]);
+//				ModulesManager.run('AdminPanelClient', 'registerAdminPanelTab', [
+//					function () { return require('modules/CoreClient/js/views/LicensingAdminSettingsView.js'); },
+//					Settings.HashModuleName + '-licensing',
+//					TextUtils.i18n('CORECLIENT/LABEL_LICENSING_SETTINGS_TAB')
+//				]);
 				ModulesManager.run('AdminPanelClient', 'registerAdminPanelTab', [
 					function () { return require('modules/CoreClient/js/views/SecurityAdminSettingsView.js'); },
 					Settings.HashModuleName + '-security',
 					TextUtils.i18n('CORECLIENT/LABEL_SECURITY_SETTINGS_TAB')
 				]);
-				ModulesManager.run('AdminPanelClient', 'registerAdminPanelTab', [
-					function () { return require('modules/CoreClient/js/views/LoggingAdminSettingsView.js'); },
-					Settings.HashModuleName + '-logging',
-					TextUtils.i18n('CORECLIENT/LABEL_LOGGING_SETTINGS_TAB')
-				]);
+//				ModulesManager.run('AdminPanelClient', 'registerAdminPanelTab', [
+//					function () { return require('modules/CoreClient/js/views/LoggingAdminSettingsView.js'); },
+//					Settings.HashModuleName + '-logging',
+//					TextUtils.i18n('CORECLIENT/LABEL_LOGGING_SETTINGS_TAB')
+//				]);
 			}
 		};
 	}
