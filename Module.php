3930915c859d481b185f4b698bdf4bed3468c834
sash04ek <a.ovcharov@gmail.com>
2017-03-07 11:21:38 +0300

    added

--- Module.php
+++ Module.php
@@ -20,7 +20,7 @@
 
 namespace Aurora\Modules\CoreWebclient;
 
-class Module extends \Aurora\System\Module\AbstractModule
+class Module extends \Aurora\System\Module\AbstractWebclientModule
 {
 	/**
 	 * Initializes CoreWebclient Module.
