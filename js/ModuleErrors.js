f4dfb8fcd2ddc9017ae61eb63fa918a81a134715
artemij-nemec <artem@chesnokov.pp.ua>
2018-12-29 10:39:29 +0300

    Added ability to show additional information about errors in subscriptions. Error needs to be described in SubscriptionsResult section of server response. 'Error' => [ 	'Code' //error code, 	'ModuleName' //name of  module that generates error, 	'Override' //true - main error message will be overridden by message from  subscription. 			   //false - message from subscription  will be added to main error message ]

--- js/ModuleErrors.js
+++ js/ModuleErrors.js
@@ -15,12 +15,37 @@ module.exports = {
 			iErrorCode = typeof oResponse.ErrorCode !== 'undefined' ? oResponse.ErrorCode : null,
 			sModuleName = typeof oResponse.Module !== 'undefined' ? oResponse.Module : null
 		;
-		
+
 		if (iErrorCode !== null && sModuleName !== null
 			&& typeof oErrors[sModuleName] !== 'undefined' 
 			&& typeof oErrors[sModuleName][iErrorCode] !== 'undefined')
 		{
 			mResult = oErrors[sModuleName][iErrorCode];
+		}
+		//Check SubscriptionsResult for error messages
+		if (oResponse.SubscriptionsResult)
+		{
+			for (var sSubscriptionIndex in oResponse.SubscriptionsResult)
+			{
+				var oSubscriptionResult = oResponse.SubscriptionsResult[sSubscriptionIndex];
+
+				if (oSubscriptionResult.Error
+					&& oSubscriptionResult.Error.Code
+					&& oSubscriptionResult.Error.ModuleName
+					&& typeof oErrors[oSubscriptionResult.Error.ModuleName] !== 'undefined' 
+					&& typeof oErrors[oSubscriptionResult.Error.ModuleName][oSubscriptionResult.Error.Code] !== 'undefined'
+				)
+				{
+					if (oSubscriptionResult.Error.Override || !mResult)
+					{
+						mResult = oErrors[oSubscriptionResult.Error.ModuleName][oSubscriptionResult.Error.Code];
+					}
+					else
+					{
+						mResult +=  "<br>" + oErrors[oSubscriptionResult.Error.ModuleName][oSubscriptionResult.Error.Code];
+					}
+				}
+			}
 		}
 		
 		if (Types.isNonEmptyString(mResult))
@@ -37,7 +62,7 @@ module.exports = {
 				mResult = sMedResult;
 			}
 		}
-		
+
 		return mResult;
 	}
 };
