9b0abec7e6928db4d562ed2c4338062257ebcbdc
nadine-shi <nadine@afterlogic.com>
2019-04-26 14:26:19 +0300

    License and other texts improving

--- Module.php
+++ Module.php
@@ -8,6 +8,8 @@
 namespace Aurora\Modules\CoreWebclient;
 
 /**
+ * System module that provides Web application core functionality and UI framework.
+ * 
  * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
  * @license https://afterlogic.com/products/common-licensing Afterlogic Software License
  * @copyright Copyright (c) 2019, Afterlogic Corp.
