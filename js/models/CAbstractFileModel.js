69754020669aab9a988fcec3cff15d298023d8db
nadine-shi <nadinshi@gmail.com>
2022-03-23 09:10:05 +0300

    Remove unnecessary code

--- js/models/CAbstractFileModel.js
+++ js/models/CAbstractFileModel.js
@@ -122,7 +122,6 @@ function CAbstractFileModel()
 	this.allowUpload = ko.observable(false);
 	this.allowPublicLink = ko.observable(false);
 	this.bIsSecure = ko.observable(false);
-	this.sharedWithOthers = ko.observable(false); // can be changed by other modules
 
 	this.sHeaderText = '';
 
