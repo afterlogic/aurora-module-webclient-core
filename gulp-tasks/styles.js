9313fa2c0118ccf15a45a2b6fc1af3848a3078af
Vasil Sokolov <vasil@afterlogic.com>
2023-03-31 10:19:23 +0200

    Fixed issue with missing styles for contacts suggestion list

--- gulp-tasks/styles.js
+++ gulp-tasks/styles.js
@@ -31,7 +31,7 @@ function BuildLibsCss(){
 	var
 		aLibsFiles = [
 			sPathToCoreWebclient + '/styles/vendors/normalize.css',
-			// sPathToCoreWebclient + '/styles/vendors/jquery/jquery-ui-1.10.4.custom.min.css',
+			sPathToCoreWebclient + '/styles/vendors/jquery/jquery-ui-1.10.4.custom.min.css',
 			//sPathToCoreWebclient + '/styles/vendors/fullcalendar-2.2.3.min.css',
 			sPathToCoreWebclient + '/styles/vendors/inputosaurus.css'
 		],
