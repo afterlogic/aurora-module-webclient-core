ae7afcc0c50f057f88b07c7eb2cec58ef813c605
Vasil Sokolov <vasil@afterlogic.com>
2023-04-02 14:35:25 +0200

    Switched to Quasar2/VueJs3

--- vue/components/CommonAdminSettings.vue
+++ vue/components/CommonAdminSettings.vue
@@ -96,7 +96,7 @@ export default {
   },
 
   beforeRouteLeave (to, from, next) {
-    this.doBeforeRouteLeave(to, from, next)
+    this.$root.doBeforeRouteLeave(to, from, next)
   },
 
   methods: {
