69768d7bb8743b226e52c86047976a391d474233
Vasil Sokolov <vasil@afterlogic.com>
2023-08-22 13:01:06 +0200

    Rolled back temporary changes

--- js/utils/Text.js
+++ js/utils/Text.js
@@ -43,18 +43,17 @@ TextUtils.plainToHtml = function (text = '', prepareLinks = false)
 	;
 
 	if (prepareLinks) {
-		console.log('plainToHtml');
 		//URLs starting with http://, https://, or ftp://
 		const replacePattern1 = /(\b(?:https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
 		html = html.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
 
-		// //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
-		// const replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
-		// html = html.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
+		//URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+		const replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+		html = html.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
 
-		// //Change email addresses to mailto:: links.
-		// const replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
-		// html = html.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
+		//Change email addresses to mailto:: links.
+		const replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
+		html = html.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
 	}
 
 	/* removing spaces befor &gt; that where added on purpose */
